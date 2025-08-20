18621e77ed0cd2882a7527d2680ea4be00b6e90a
jennyflying-25 <jennyflying.hu@gmail.com>
2025-08-20 11:48:38 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
     import { Inter } from "next/font/google";
-    
-    // Import Tailwind's core styles directly. This bypasses globals.css completely.
-    import "tailwindcss/tailwind.css";
+    import "./globals.css";
 
     const inter = Inter({ subsets: ["latin"] });
 
